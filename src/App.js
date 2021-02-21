7df63fb0a1bb681692733f81b9b02bb0b200333f
ivan wolyniec <ivan@ivans-MacBook-Pro.local>
2021-02-21 14:42:53 -0500

    broke it lmao

--- src/App.js
+++ src/App.js
@@ -1,37 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import {default as apiKey} from './apiKey.js';
 
-function Card(props){
-  let title = props.title;
-  return (
+// {  }
+function Controller(props){
+  return(
     <>
-      <h1>{title}</h1>
+      <form onSubmit={props.getWeather}>
+        <input id="zipInput" placeholder="zip, ie 48212"></input>
+        <button>+</button>
+      </form>
     </>
   );
 }
-function App() {
-  let [title, setTitle] = useState(window.localStorage.getItem("title"));
+function WeatherData(props){
+  return(
+    <>
+      <h2>{props.data.name}</h2>
+      <h2>{props.data.weather[0].description}</h2>
+      <h2>{props.data.main.temp}</h2>
+    </>
+  );
+}
 
+function App() {
+  let [zip, setZip] = useState(48212);
+  let [weatherData, setWeatherData] = useState([]);
+
+  let getWeather = e => {
+    e.preventDefault();
+    let newZip = document.getElementById("zipInput").value;
+    // VALIDATE ZIP CODE HERE
+    if(newZip.length > 1 ){
+      setZip(newZip);
+      console.log(newZip);
+      weatherFetch();
+    }
+  }
+  let weatherFetch = async _ =>{
+    try{
+     let url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${apiKey}`;
+      const weatherCall = await fetch(url)
+      .then(response => response.json())
+      .then(weather => setWeatherData([weather]));
+    } catch(e) {
+      console.error(`yikes lmao looks like ya got a problemo`);
+      console.error(e);
+    }
+  }
+
+  weatherFetch();
   return (
-    <div className="App">
+    <>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <input onChange={e => {setTitle(e.target.value); window.localStorage.setItem("title",title)}}></input>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a href="">
+          <h1>Weather</h1>
         </a>
-        <Card title={title} />
+        <Controller getWeather={getWeather}/>
+        <h2>Currently weatherin' out <i>{zip}</i></h2>
       </header>
-    </div>
+      <div>
+        {weatherData.map((e) => (<WeatherData data={e}/>))}
+      </div>
+    </>
   );
 }
 
